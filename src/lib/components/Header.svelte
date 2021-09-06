3476d51fb0683eadab8e487bf102e248ff1be4d6
erxn-mcg <87203096+erxn-mcg@users.noreply.github.com>
2021-09-06 20:44:41 +0100

    Update Header.svelte

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -42,12 +42,12 @@
 							</a>
 							<a
 								class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-								href="/"
+								href="/contact"
 							>
 								Contact
 							</a><a
 								class="text-gray-300  hover:text-gray-800 dark:hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-								href="/"
+								href="/about"
 							>
 								About
 							</a>
